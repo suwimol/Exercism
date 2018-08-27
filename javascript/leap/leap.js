c7055dc072ae207ec50106cf0a86f2bce5c28fd0
Suwimol Kamlangjai <skamlangjai@skamlangjai-mbp.bcpress.lan>
2018-08-27 09:57:38 -0400

    create a constructor for leap class

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,8 +1,12 @@
 class Leap {
-  isLeap(year) {
+  Leap(year) {
+    this.year = year;
+  }
+
+  isLeap() {
     // the given year is a leap year only if it is divisible by 4 or 
     // if it is both divisible by 100 and 400
-    return (year % 4) || (year % 100 == 0 && year % 400 == 0)
+    return (this.year % 4) || (this.year % 100 == 0 && this.year % 400 == 0)
   }
 }
 
