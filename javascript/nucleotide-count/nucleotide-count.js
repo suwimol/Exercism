c718be9a1dbf1e9a14e5031b0f09bab817119b74
Suwimol Kamlangjai <meieing@gmail.com>
2018-09-07 11:16:50 -0400

    completed nucleotide-count exercise

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,5 +1,22 @@
+var result = '';
+const nucleotides = [/A/g, /C/g, /G/g, /T/g];
+
 class NucleotideCounts {
-  parse(string) {
-    
+  static parse(strand) {
+    if (strand.replace(/A|C|G|T/g, '').length) {
+      throw new Error('Invalid nucleotide in strand');
+    }
+
+    for (var i = 0; i < nucleotides.length; i++) {
+      var count = 0;
+      if (strand.match(nucleotides[i]) != null) {
+        count = strand.match(nucleotides[i]).length;
+      }
+      result += count + ' ';
+    }
+    // remove space at the end of the result string
+    result = result.substring(0, result.lastIndexOf(' '));
+    return result;
   }
 }
+export default NucleotideCounts;
